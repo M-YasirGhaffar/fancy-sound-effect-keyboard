7b49f645cb0f53dad2dcfff3d47d425d7e9287c4
Muhammad Yasir Ghaffar <mghaffar.bese21seecs@seecs.edu.pk>
2023-08-20 03:27:27 +0500

    nsfw meant for fun

---
+++ script.js
@@ -0,0 +1,58 @@
+const keys = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+const timestamps = [];
+
+timestamps.unshift(getTimestamp());
+
+function getRandomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomKey() {
+  return keys[getRandomNumber(0, keys.length-1)]
+}
+
+function targetRandomKey() {
+  const key = document.getElementById(getRandomKey());
+  key.classList.add("selected");
+  let start = Date.now()
+}
+
+function getTimestamp() {
+  return Math.floor(Date.now() / 1000)
+}
+
+
+
+document.addEventListener("keyup", event => {
+  const keyPressed = String.fromCharCode(event.keyCode);
+  console.log(keyPressed)
+  const keyElement = document.getElementById(keyPressed);
+  console.log(keyElement)
+  const highlightedKey = document.querySelector(".selected");
+  console.log(highlightedKey)
+  
+  keyElement.classList.add("hit")
+  keyElement.addEventListener('animationend', () => {
+    keyElement.classList.remove("hit")
+  })
+  
+  if (keyPressed === highlightedKey.innerHTML) {
+    timestamps.unshift(getTimestamp());
+    const elapsedTime = timestamps[0] - timestamps[1];
+    console.log(`Character per minute ${60/elapsedTime}`)
+    highlightedKey.classList.remove("selected");
+    targetRandomKey();
+    const success = new Audio('sounds/correct.wav');
+    success.play();
+  } else {
+    let randNum = Math.floor(Math.random()*9) + 1
+    const errorAudio = new Audio(`sounds/error${randNum}.mp3`);
+    errorAudio.play();
+  }
+})
+
+
+targetRandomKey();
